cea4b23d1126d0c509d8689869375aeaf0afe470
Faanilo <faniloniainaa21@gmail.com>
2022-12-02 23:38:38 +0300

    backend

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+CORS(app)
 
-#Route voloany
+'''
+Db configuration
+
+'''
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/pharma-tek'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+ma = Marshmallow(app)
+
+
+class Medocs:
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    price = db.Column(db.Integer)
+
+
+class MedocsSchema:
+    class Meta:
+        fields = ('id', 'name', 'price')
+# Route voloany
+
+
 @app.route('/')
 def home():
-    return('gg , ao amle API tsika ')
+    return ('gg , ao amle API tsika ')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
