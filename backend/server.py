a19795978126f0c2fee5e53dc6a36dc07a589d02
Faanilo <faniloniainaa21@gmail.com>
2022-12-03 23:02:27 +0300

    backend

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
 import pymysql
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
@@ -22,7 +22,7 @@ ma = Marshmallow(app)
 class Medocs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
-    price = db.Column(db.Integer)
+    price = db.Column(db.String(100))
 
     def __init__(self, name, price):
         self.name = name
@@ -34,7 +34,7 @@ class MedocsSchema(ma.Schema):
         fields = ('id', 'name', 'price')
 
 
-medocs_schema = MedocsSchema()
+medoc_schema = MedocsSchema()
 medocs_schema = MedocsSchema(many=True)
 # Route voloany
 
@@ -49,6 +49,17 @@ def get_medocs():
     all_medocs = Medocs.query.all()
     results = medocs_schema.dump(all_medocs)
     return jsonify(results)
+
+
+@app.route('/add',methods=['POST'])
+def add_medocs():
+    name = request.json['name']
+    price = request.json['price']
+
+    meds = Medocs(name, price)
+    db.session.add(meds)
+    db.session.commit()
+    return medocs_schema.jsonify(meds)
 
 
 if __name__ == '__main__':
