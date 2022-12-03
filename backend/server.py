262d780894839365ddfad49db9ab0f12a05b1bba
Faanilo <faniloniainaa21@gmail.com>
2022-12-03 22:24:12 +0300

    backend

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify, request
+import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_marshmallow import Marshmallow
@@ -10,9 +11,10 @@ CORS(app)
 Db configuration
 
 '''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/pharma-tek'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:''@localhost/pharma-tek'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 ma = Marshmallow(app)
 
